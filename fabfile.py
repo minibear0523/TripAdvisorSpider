b7c1b615f352c8a744c41bb0200dd9d4b66d820c
minibear <lei.zhang.bupt08@gmail.com>
2016-08-06 18:56:57 +0800

    MOD:增加fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,26 @@
 Topic: Fabric Automatic Deploy File
 Desc: 
 """
-from fabric.api import local
+from __future__ import with_statement
+from fabric.api import local, settings, abort, run, cd, env
+from fabric.contrib.console import confirm
 
+
+env.hosts = ['192.168.1.4']
+env.warn_only = True
+env.user = 'minibear'
 
 def prepare_deploy():
     local('git add --all && git commit')
     local('git push')
 
+
+def deploy():
+    code_dir = '/home/minibear/projects/TripAdvisorSpider'
+    with cd(code_dir):
+        run('git pull')
+
+
+def d():
+    prepare_deploy()
+    deploy()
